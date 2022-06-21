b4a7f62e8cc29c2978e98e993b6d485b9a299276
mansimishra123 <95296459+mansimishra123@users.noreply.github.com>
2022-06-21 05:40:13 -0700

    Add files via upload

---
+++ toy_gun.js
@@ -0,0 +1,41 @@
+status=""
+
+function setup(){
+    canvas=createCanvas(640,420)
+    canvas.center()
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded)
+    document.getElementById("status").innerHTML="Status: detecting objects "
+}
+
+function preload(){
+    img=loadImage("toy_gun.jpg")
+}
+
+function modelLoaded(){
+    console.log("modelLoaded")
+status=true
+objectDetector.detect(img,gotResult)
+}
+
+function draw(){
+    image(img,0,0,640,420)
+    fill("white")
+    text("TOY_GUN",45,75)
+    noFill()
+    stroke("red")
+    rect(30,60,450,350)
+
+  
+}
+
+function gotResult(error,result){
+    if(error){
+        console.log(error)
+    }
+    console.log(result)
+
+}
+
+function back(){
+    window.location="index.html"
+}
